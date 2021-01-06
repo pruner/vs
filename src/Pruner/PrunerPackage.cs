1537c0cfa992be5f6d456d530b7bfd962ea1e9e9
Mathias Lorenzen <mathias.lorenzen@live.com>
2021-01-06 22:13:23 +0100

    fix for package.

--- src/Pruner/PrunerPackage.cs
+++ src/Pruner/PrunerPackage.cs
@@ -6,7 +6,6 @@ using System.IO;
 using System.Runtime.InteropServices;
 using System.Threading;
 using Task = System.Threading.Tasks.Task;
-using Microsoft.VisualStudio.Workspace;
 
 namespace Pruner
 {
