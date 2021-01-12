8fab7b9dff937e549fbf62ee7fa9c21068d7753e
Mathias Lorenzen <mathias.lorenzen@live.com>
2021-01-12 20:49:35 +0100

    bugfix for test navigation.

--- src/Pruner/UI/Window/TestsWindowControl.xaml.cs
+++ src/Pruner/UI/Window/TestsWindowControl.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Diagnostics.CodeAnalysis;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Windows;
 using System.Windows.Controls;
 using EnvDTE;
@@ -43,17 +44,20 @@ namespace Pruner.UI.Window
 
                 var projects = dte.Solution.Projects
                     .Cast<Project>()
+                    .Where(x => x != null)
                     .ToArray();
                 var codeModels = projects
                     .Select(x => x.CodeModel)
                     .Cast<CodeModel2>()
+                    .Where(x => x != null)
                     .ToArray();
                 var codeElements = codeModels
                     .Select(x => x.CodeTypeFromFullName(test.FullClassName))
                     .Where(x => x != null)
                     .SelectMany(x => x
                         .Members
-                        .Cast<CodeElement2>())
+                        .Cast<CodeElement2>()
+                        .Where(f => f != null))
                     .ToArray();
                 var matchingMembers = codeElements
                     .Where(x => x.FullName == test.FullName)
@@ -63,9 +67,13 @@ namespace Pruner.UI.Window
 
                 foreach (var member in matchingMembers)
                 {
-                    OutputLogger.Log("Opening document", member.ProjectItem.FileNames[0]);
+                    var projectItem = GetProjectItemFromCodeElement(member);
+                    if (projectItem == null || projectItem.FileCount == 0)
+                        continue;
 
-                    var window = member.ProjectItem.Open(Constants.vsViewKindCode);
+                    OutputLogger.Log("Opening document", projectItem.FileNames[0]);
+
+                    var window = projectItem.Open(Constants.vsViewKindCode);
                     window.Visible = true;
 
                     var selection = window.Document.Selection as TextSelection;
@@ -76,6 +84,24 @@ namespace Pruner.UI.Window
             catch (Exception ex)
             {
                 OutputLogger.Log("An error occured while trying to navigate to a test", ex);
+            }
+        }
+
+        private static ProjectItem GetProjectItemFromCodeElement(CodeElement2 member)
+        {
+            try
+            {
+                return member.ProjectItem;
+            }
+            catch (COMException ex)
+            {
+                if (ex.ErrorCode == -2147467259)
+                {
+                    //this may happen in certain scenarios. not sure why yet.
+                    return null;
+                }
+
+                throw;
             }
         }
     }
